056fe7fd49ee98dba894eb583e0565a0796e79a0
Santiago Chaves <santichavesg@gmail.com>
2023-11-15 20:08:41 -0600

    20 arcos y 5 ciclos listos

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "grafo.h"
 #include "Lugar.h"
 #include "GraphDiscover.h"
+#include "QuestionSolver.h"
 #include <vector>
 #include <string>
 using namespace std;
@@ -39,44 +40,59 @@ int main()
     }
     //peso es el tiempo en horas llegando en tren
 
-    grafo.addArc(&venezuela,&colombia, 4);
-    grafo.addArc(&argentina, &uruguay, 8);
-    grafo.addArc(&argentina, &chile, 3);
+    grafo.addArc(&argentina,&uruguay, 2);   //primer cierre de argetina-uruguay-brasil-bolivia-paraguay-argentina
+    grafo.addArc(&uruguay,&argentina, 2);
+    grafo.addArc(&argentina,&chile, 4);
+    grafo.addArc(&chile,&argentina, 4);
+    grafo.addArc(&argentina,&bolivia, 5);
+    grafo.addArc(&bolivia,&brasil, 3);
+    grafo.addArc(&uruguay,&brasil, 1);
+    grafo.addArc(&brasil,&bolivia, 2);
+    grafo.addArc(&bolivia,&paraguay, 2);
+    grafo.addArc(&paraguay,&argentina, 2);
+    grafo.addArc(&bolivia,&peru, 4);
+    grafo.addArc(&chile,&peru, 3);
+    grafo.addArc(&peru,&ecuador, 2);      //segundo cierre de peru-ecuador-colombia-venezuela-brasil-peru
+    grafo.addArc(&peru,&brasil, 2);
+    grafo.addArc(&brasil,&colombia, 2);
+    grafo.addArc(&brasil,&peru, 2);
+    grafo.addArc(&ecuador,&colombia, 2);
+    grafo.addArc(&colombia,&panama, 20);
+    grafo.addArc(&colombia,&venezuela, 2);
+    grafo.addArc(&venezuela,&brasil, 2);
+    grafo.addArc(&panama,&costaRica, 2);
+    grafo.addArc(&costaRica,&nicaragua, 2);
+    grafo.addArc(&nicaragua,&honduras, 2);
+    grafo.addArc(&honduras,&elSalvador, 2);
+    grafo.addArc(&elSalvador,&guatemala, 2);
+    grafo.addArc(&guatemala,&mexico, 2);
+    grafo.addArc(&mexico,&estadosUnidos, 2);
     
 
-    grafo.addArc(&uruguay,&brasil, 3);
-    grafo.addArc(&uruguay,&paraguay,3);
 
-    grafo.addArc(&paraguay,&bolivia, 7);
-    grafo.addArc(&chile,&bolivia, 10);
-    grafo.addArc(&bolivia,&peru, 14);
 
-    grafo.addArc(&peru,&ecuador, 5);
-    grafo.addArc(&ecuador,&colombia, 1);
-    grafo.addArc(&peru,&colombia, 2);
-
-    grafo.addArc(&colombia,&panama, 40);
-    grafo.addArc(&panama,&costaRica, 10);
-    grafo.addArc(&costaRica,&nicaragua,3 );
-    grafo.addArc(&nicaragua,&honduras, 6);
-    grafo.addArc(&honduras,&elSalvador, 5);
-    grafo.addArc(&elSalvador,&guatemala, 7);
-    grafo.addArc(&guatemala,&mexico, 8);
-    grafo.addArc(&estadosUnidos,&mexico, 2);
-    grafo.addArc(&mexico,&estadosUnidos, 2);
 
 
     GraphDiscover gd = GraphDiscover();
 
     NodoGrafo *nodo1 = grafo.getNodo(argentina.getId());
-    NodoGrafo *nodo2 = grafo.getNodo(estadosUnidos.getId());
+    NodoGrafo *nodo2 = grafo.getNodo(peru.getId());
 
-    Path * newPath = gd.getShorterPath(nodo1, nodo2, &grafo);
+    QuestionSolver qs = QuestionSolver();
 
+//DJISTRA:
+    // qs.printLeastTimeFromOneCOuntryToOther(nodo1, nodo2, &grafo);
 
-    for(int k = 0; k < newPath->path.size(); k++){
-        cout <<newPath->path.at(k)->getInfo()->getNombre()  << endl;
-    }
+//WARSHALL:
+    vector<Path *> *wasrshalPaths = gd.getPathsByWarshall(nodo1, nodo2, &grafo);
+
+    // for(int j = 0 ; j < wasrshalPaths->size(); j++){
+        
+    //     for(int k = 0; k < wasrshalPaths->at(j)->path.size(); k++){
+    //         cout <<wasrshalPaths->at(j)->path.at(k)->getInfo()->getNombre()  << endl;
+    //     }
+    //     cout << "----------------" << endl;
+    // }
     // vector<INodo *> result = grafo.broadPath(&estadosUnidos);
     // for (int i = 0; i < result.size(); i++)
     // {
